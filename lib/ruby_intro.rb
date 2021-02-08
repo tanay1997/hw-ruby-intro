d244ea6f6ac7b072ac81921054d54257ed794373
EC2 Default User <ec2-user@ip-172-31-29-151.us-east-2.compute.internal>
2021-02-08 20:05:42 +0000

    First draft

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,145 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  ans = 0
+  for num in arr
+    ans += num
+  end
+  return ans
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  # Array with length = 0
+  if arr.empty?
+    return 0
+  end
+
+  # Array with length = 1
+  if arr.length == 1
+      return arr[-1]
+  end
+
+  # Array with length > 2
+  ans = 0
+  sortedArr = arr.sort
+  ans = sortedArr[-1] + sortedArr[-2]
+  return ans
+
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  # Base condition
+  if arr.length < 2
+   return false
+  end 
+
+  # Array with length > 2
+  sortedArr = arr.sort
+  head = 0
+  tail = arr.length - 1
+
+  while head < tail
+    result = sortedArr[head] + sortedArr[tail]
+
+    if (result == n)
+      return true
+    end
+
+    if (result < n)
+      head += 1
+    else
+      tail -= 1
+    end
+  end
+
+  return false
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  # Base conditions
+  if (s.length == 0 || s.match?(/[^a-zA-Z]/))
+    return false
+  end
+
+  regVar = /\A[^aeiouAEIOU]/i 
+  return s.match?(regVar)
+
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # Base condition
+  if  (s.length == 0 || s.match?(/[^01]/i))  
+     return false
+  end
+
+  # Binary to Decimal conversion
+  decimalNumber = 0
+  s.split("").each do |i|
+    decimalNumber += (i == "1") ? 1 : 0
+    decimalNumber *= 2
+  end
+
+  if (decimalNumber % 4 == 0)
+   return true
+  end
+
+  return false
+
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  # Constructor Initialization
+  def initialize(isbn, price)
+
+    if (price == nil || price <= 0)
+      raise ArgumentError, 'Invalid price'
+    end
+
+    if (isbn == nil || isbn.size == 0)
+      raise ArgumentError, 'Invalid ISBN'
+    end
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def isbn=(isbn)
+    # Base condition
+    if (isbn == nil || isbn.size == 0)
+      raise ArgumentError, 'Invalid ISBN'
+    end
+
+    @isbn = isbn
+  end
+
+  def price=(price)
+    # Base condition
+    if (price == nil || price <= 0)
+      raise ArgumentError, 'Invalid price'
+    end
+
+    @price = price
+  end
+
+  def isbn
+    @isbn
+  end
+
+  def price
+    @price
+  end
+
+  def price_as_string
+    displayPrice ="%.2f" %  @price
+    return "$"+displayPrice
+  end
 end
